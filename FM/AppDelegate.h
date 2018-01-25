0671b4530414094114e379fd5f49ea8b8c67a001
wong sam <wongsam@wongdeMacBook-Pro.local>
2018-01-25 21:50:59 +0800

    注释

---
+++ FM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM
+//
+//  Created by wong sam on 2018/1/25.
+//  Copyright © 2018年 wong sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
